e9ff02e775862e444ede946401ff70748f7da845
ArcheanChaos <summerlius@2980.com>
2021-02-23 10:11:54 +0800

    changes for v1.1.3

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/summerlius/liusmod
+module github.com/ArcheanChaos/liusmod
 
 go 1.15
--- pkg1/pkg1.go
+++ pkg1/pkg1.go
@@ -4,6 +4,6 @@ import "fmt"
 
 // Hello name
 func Hello(name string) string {
-	fmt.Println("v1.1.0")
+	fmt.Println("v1.1.3")
 	return name
 }
